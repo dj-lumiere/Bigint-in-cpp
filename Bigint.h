3d3afea696639d14d24966c5dfe1e57123dd14d0
DJ_Lumiere <djminstar573@gmail.com>
2023-08-01 18:49:59 +0900

    Update Bigint.h

--- Bigint.h
+++ Bigint.h
@@ -4,161 +4,239 @@
 #include <string>
 #include <algorithm>
 #include <cmath>
+#include <type_traits>
+#include <numeric>
 
 namespace ntt
 {
-    const int32_t MOD = 998244353;
-    const int32_t ROOT = 3;
-    int32_t multiply_mod(int32_t a, int32_t b)
+    const uint32_t INT32_BITMASK = 0xffffffff;
+    const uint32_t MOD = 2013265921;
+    const uint64_t MOD_MAGIC = 2459565875272927297;
+    const uint32_t MOD_MULINV = 21;
+    const uint32_t MOD_SHIFT = 28;
+    const uint32_t ROOT = 31;
+
+    uint64_t multiply_int128_high(uint64_t a, uint64_t b);
+    int32_t find_bit_ceil(int32_t a);
+    void bit_reversal_permutation(std::vector<int32_t> &a);
+    std::vector<int32_t> polynomial_multiplication(const std::vector<int32_t> &a, const std::vector<int32_t> &b);
+
+    int32_t multiply_mod(int32_t a, int32_t b);
+    int32_t power_with_mod(int32_t base, int32_t index);
+    int32_t modular_inverse(int32_t base);
+    void coefficient_normalization1(std::vector<int32_t> &a);
+    void transform_mod(std::vector<int32_t> &a, bool invert);
+}
+
+uint64_t ntt::multiply_int128_high(uint64_t a, uint64_t b)
+{
+    register uint64_t a_high = a >> 32;
+    register uint64_t a_low = a & INT32_BITMASK;
+    register uint64_t b_high = b >> 32;
+    register uint64_t b_low = b & INT32_BITMASK;
+    register uint64_t product_high = a_high * b_high;
+    register uint64_t product_low = a_low * b_low >> 32;
+    register uint64_t product_mid1 = a_high * b_low;
+    register uint64_t product_mid2 = a_low * b_high;
+    register uint64_t product_mid_high = product_mid1 >> 32 + product_mid2 >> 32;
+    register uint64_t product_mid_low = product_mid1 & INT32_BITMASK + product_mid2 & INT32_BITMASK;
+    uint64_t carry = 0;
+    product_high += product_mid_high;
+    product_low += product_mid_low;
+    carry = product_low >> 32;
+    product_high += carry; // Carry from the low 64 bits
+    return product_high;
+}
+int32_t ntt::find_bit_ceil(int32_t a)
+{
+    return static_cast<int32_t>(ceil(log2(a)));
+}
+void ntt::bit_reversal_permutation(std::vector<int32_t> &a)
+{
+    int32_t n = a.size();
+    for (int32_t i = 1, j = 0; i < n; ++i)
     {
-        return static_cast<int32_t>((static_cast<int64_t>(a) * b) % MOD);
-    }
-    int32_t power_with_mod(int32_t base, int32_t index)
-    {
-        int32_t answer = 1;
-        while (index)
+        int32_t bit = n >> 1;
+        for (; j & bit; bit >>= 1)
         {
-            if (index & 1)
-            {
-                answer = multiply_mod(answer, base);
-            }
-            base = multiply_mod(base, base);
-            index >>= 1;
-        }
-        return answer;
-    }
-    int32_t modular_inverse(int32_t base)
-    {
-        return power_with_mod(base, MOD - 2);
-    }
-    int32_t find_bit_ceil(int32_t a)
-    {
-        return static_cast<int32_t>(ceil(log2(a)));
-    }
-    void bit_reversal_permutation(std::vector<int32_t> &a)
-    {
-        int32_t n = a.size();
-        for (int32_t i = 1, j = 0; i < n; ++i)
-        {
-            int32_t bit = n >> 1;
-            for (; j & bit; bit >>= 1)
-            {
-                j ^= bit;
-            }
             j ^= bit;
-            if (i < j)
-            {
-                std::swap(a[i], a[j]);
-            }
+        }
+        j ^= bit;
+        if (i < j)
+        {
+            std::swap(a[i], a[j]);
         }
     }
-    void coefficient_normalization(std::vector<int32_t> &a)
+}
+std::vector<int32_t> ntt::polynomial_multiplication(const std::vector<int32_t> &a, const std::vector<int32_t> &b)
+{
+    int32_t original_size = a.size() + b.size();
+    int32_t n = 1 << ntt::find_bit_ceil(original_size);
+
+    std::vector<int32_t> transform_a_mod(a.begin(), a.end());
+    transform_a_mod.resize(n);
+    ntt::transform_mod(transform_a_mod, false);
+
+    std::vector<int32_t> transform_b_mod(b.begin(), b.end());
+    transform_b_mod.resize(n);
+    ntt::transform_mod(transform_b_mod, false);
+
+    for (int32_t i = 0; i < n; i++)
     {
-        int32_t n = a.size();
-        int32_t n_inv = modular_inverse(n);
-        for (int32_t i = 0; i < n; ++i)
-        {
-            a[i] = multiply_mod(a[i], n_inv);
-        }
+        transform_a_mod[i] = multiply_mod(transform_a_mod[i], transform_b_mod[i]);
     }
-    void transform(std::vector<int32_t> &a, bool invert)
+
+    ntt::transform_mod(transform_a_mod, true);
+    transform_a_mod.resize(original_size);
+
+    return transform_a_mod;
+}
+
+int32_t ntt::multiply_mod(int32_t a, int32_t b)
+{
+    uint64_t product = static_cast<uint64_t>(a) * b;
+    uint64_t quotient = multiply_int128_high(product, MOD_MAGIC) >> MOD_SHIFT;
+    uint64_t result = product - quotient * MOD;
+    if (result >= MOD)
     {
-        int32_t n = a.size();
-        bit_reversal_permutation(a);
-        std::vector<int32_t> root_of_unity(n / 2, 0);
-        int32_t angle;
-        if (invert)
+        result -= MOD;
+    }
+    return static_cast<int32_t>(result);
+}
+int32_t ntt::power_with_mod(int32_t base, int32_t index)
+{
+    int32_t answer = 1;
+    while (index > 0)
+    {
+        if (index & 1)
         {
-            angle = MOD - 1 - (MOD - 1) / n;
+            answer = multiply_mod(answer, base);
         }
-        else
+        base = multiply_mod(base, base);
+        index >>= 1;
+    }
+    return answer;
+}
+int32_t ntt::modular_inverse(int32_t base)
+{
+    return power_with_mod(base, MOD - 2);
+}
+void ntt::coefficient_normalization1(std::vector<int32_t> &a)
+{
+    int32_t n = a.size();
+    int32_t n_inv = modular_inverse(n);
+    for (int32_t i = 0; i < n; ++i)
+    {
+        a[i] = multiply_mod(a[i], n_inv);
+    }
+}
+void ntt::transform_mod(std::vector<int32_t> &a, bool invert)
+{
+    int32_t n = a.size();
+    bit_reversal_permutation(a);
+    std::vector<int32_t> root_of_unity(n / 2, 0);
+    int32_t angle = (MOD - 1) / n;
+    if (invert)
+    {
+        angle = MOD - 1 - angle;
+    }
+    root_of_unity[0] = 1;
+    int32_t angleth_power = power_with_mod(ROOT, angle);
+    for (int32_t i = 1; i < n / 2; ++i)
+    {
+        root_of_unity[i] = multiply_mod(root_of_unity[i - 1], angleth_power);
+    }
+    for (int32_t len = 2; len <= n; len <<= 1)
+    {
+        int32_t step = n / len;
+        for (int32_t i = 0; i < n; i += len)
         {
-            angle = (MOD - 1) / n;
-        }
-        root_of_unity[0] = 1;
-        int32_t angleth_power = power_with_mod(ROOT, angle);
-        for (int32_t i = 1; i < n / 2; ++i)
-        {
-            root_of_unity[i] = multiply_mod(root_of_unity[i - 1], angleth_power);
-        }
-        for (int32_t len = 2; len <= n; len <<= 1)
-        {
-            int32_t step = n / len;
-            for (int32_t i = 0; i < n; i += len)
+            for (int32_t j = 0; j < len / 2; j++)
             {
-                for (int32_t j = 0; j < len / 2; j++)
+                int32_t u = a[i + j];
+                int32_t v = multiply_mod(a[i + j + len / 2], root_of_unity[step * j]);
+                a[i + j] = u + v;
+                a[i + j + len / 2] = u - v;
+                if (u + v >= MOD)
                 {
-                    int32_t u = a[i + j];
-                    int32_t v = multiply_mod(a[i + j + len / 2], root_of_unity[step * j]);
-                    if (u + v < MOD)
-                    {
-                        a[i + j] = u + v;
-                    }
-                    else
-                    {
-                        a[i + j] = u + v - MOD;
-                    }
-                    if (u - v >= 0)
-                    {
-                        a[i + j + len / 2] = u - v;
-                    }
-                    else
-                    {
-                        a[i + j + len / 2] = u - v + MOD;
-                    }
+                    a[i + j] -= MOD;
+                }
+                if (u - v < 0)
+                {
+                    a[i + j + len / 2] += MOD;
                 }
             }
         }
-        if (invert)
-        {
-            coefficient_normalization(a);
-        }
     }
-    std::vector<int32_t> polynomial_multiplication(const std::vector<int32_t> &a, const std::vector<int32_t> &b)
+    if (invert)
     {
-        int32_t original_size = a.size() + b.size();
-        std::vector<int32_t> transformed_a1(a.begin(), a.end());
-        std::vector<int32_t> transformed_b1(b.begin(), b.end());
-        int32_t n = 1 << ntt::find_bit_ceil(original_size);
-        transformed_a1.resize(n);
-        transformed_b1.resize(n);
-        ntt::transform(transformed_a1, false);
-        ntt::transform(transformed_b1, false);
-        for (int32_t i = 0; i < n; i++)
-        {
-            transformed_a1[i] = ntt::multiply_mod(transformed_a1[i], transformed_b1[i]);
-        }
-        ntt::transform(transformed_a1, true);
-        transformed_a1.resize(original_size);
-        return transformed_a1;
+        coefficient_normalization1(a);
     }
 }
 
+
+/**
+ * @class BigInt
+ *
+ * The BigInt class provides arbitrary-precision arithmetic on integers.
+ * BigInt can be used to handle very large numbers that are beyond the capacity of standard integer types in C++.
+ *
+ * The class supports standard arithmetic operations like addition, subtraction, multiplication, division, and also modulo operations.
+ * Relational and equality operations are also supported, as well as some utility functions such as absolute value and greatest common divisor.
+ *
+ * BigInt can be constructed from 1) integer types and 2) from strings. When constructed from a string, the string must consist only of digits, with an optional leading minus sign for negative numbers.
+ *
+ * The implementation handles signs and trailing zeros internally and supports automatic normalization, i.e., removing leading zeros from the internal representation.
+ *
+ * The BigInt class supports input/output via C++ streams, and the output is always in base 10.
+ *
+ * Note: BigInt only supports only integers and does not support floating-point numbers.
+ */
 class BigInt
 {
 private:
+    // Constants for sign representation
     static const int32_t PLUS = 1;
+    // Constants for sign representation
     static const int32_t MINUS = -1;
+    // Constants for sign representation
     static const int32_t ZERO = 0;
+    // Sign of the number, default is PLUS
     int32_t sign = 1;
+    // Number of digits in the BigInt
     int32_t digit = 1;
+    // Digits of the BigInt stored in a vector
     std::vector<int32_t> mantissa;
 
 public:
+    // Constructors
+    // Construct from string
     BigInt(const std::string &target);
+    // Construct from C-string
     BigInt(const char *target);
+    // Construct from sign and digit vector
     BigInt(int32_t sign, const std::vector<int32_t> &mantissa);
-    BigInt(int64_t target);
-    BigInt(int32_t target);
+    // Construct from integral types
+    template <typename T>
+    BigInt(T target);
 
+    // Comparison operators
     bool operator==(const BigInt &other);
+    // Comparison operators
     bool operator!=(const BigInt &other);
+    // Comparison operators
     bool operator>=(const BigInt &other);
+    // Comparison operators
     bool operator>(const BigInt &other);
+    // Comparison operators
     bool operator<=(const BigInt &other);
+    // Comparison operators
     bool operator<(const BigInt &other);
 
+    // Unary arithmetic operators
     BigInt operator-() const;
+
+    // Binary arithmetic operators
     BigInt operator+(const BigInt &other);
     BigInt operator++(int32_t a);
     BigInt operator-(const BigInt &other);
@@ -167,6 +245,7 @@ public:
     BigInt operator/(const BigInt &other);
     BigInt operator%(const BigInt &other);
 
+    // Arithmetic assignment operators
     BigInt &operator+=(const BigInt &other);
     BigInt &operator++();
     BigInt &operator-=(const BigInt &other);
@@ -175,38 +254,61 @@ public:
     BigInt &operator/=(const BigInt &other);
     BigInt &operator%=(const BigInt &other);
 
+    // Getters
     int32_t get_sign() const;
     int32_t get_digit() const;
     std::vector<int32_t> get_mantissa() const;
 
+    // I/O operators
     friend std::istream &operator>>(std::istream &is, BigInt &target);
     friend std::ostream &operator<<(std::ostream &os, const BigInt &target);
 
+    // Get absolute value
     friend BigInt abs(BigInt target);
+    // Division and modulus operation
     friend std::vector<BigInt> divmod(BigInt target1, BigInt target2);
+    // Greatest Common Divisor of two BigInts
     friend BigInt gcd(BigInt target1, BigInt target2);
+    // Least Common Multiple of two BigInts
     friend BigInt lcm(BigInt target1, BigInt target2);
+    // Integer square root of BigInt
     friend BigInt isqrt(BigInt target);
+    // Integer cube root of BigInt
     friend BigInt icbrt(BigInt target);
+    // Factorial of BigInt
     friend BigInt factorial(BigInt target);
+    // Raise base to the power index
     friend BigInt pow(BigInt base, BigInt index);
+    // Raise base to the power index, modulo mod
     friend BigInt pow(BigInt base, BigInt index, BigInt mod);
+    // Multiplicative inverse of target modulo mod
     friend BigInt multiplicative_inverse(const BigInt &target, const BigInt &mod);
+    // Maximum of two BigInts
     friend BigInt max(BigInt target1, BigInt target2);
+    // Minimum of two BigInts
     friend BigInt min(BigInt target1, BigInt target2);
 
 protected:
+    // Function to remove trailing zeros in mantissa
     void remove_trailing_zero(std::vector<int32_t> &target);
 
+    // Functions for internal use
+    // Check if this BigInt is smaller than other
     bool is_smaller_than_other(const BigInt &other);
+    // Check if this BigInt is smaller than other, when they have the same number of digits
     bool is_smaller_than_other_same_digit(const BigInt &other);
-
+    // Add the mantissa of two BigInts
     BigInt add_mantissa(const BigInt &other);
+    // Subtract the mantissa of other from this BigInt
     BigInt subtract_mantissa(const BigInt &other);
+    // Multiply the mantissa of two BigInts
     BigInt multiply_mantissa(const BigInt &other);
+    // Division and modulus operation on mantissa
     std::vector<BigInt> divmod_mantissa(const BigInt &other);
+    // Division and modulus operation
     std::vector<BigInt> divmod(const BigInt &other);
 
+    // Function for Extended Euclidean Algorithm
     friend std::vector<BigInt> extended_euclidean(BigInt a, BigInt b, BigInt d);
 };
 
@@ -260,31 +362,10 @@ BigInt::BigInt(const char *target)
     }
     this->digit = this->mantissa.size();
 }
-BigInt::BigInt(int64_t target)
+template <typename T>
+BigInt::BigInt(T target)
 {
-    this->mantissa.clear();
-    this->sign = PLUS;
-    if (target == 0)
-    {
-        this->sign = ZERO;
-        this->mantissa = {0};
-        this->digit = 1;
-        return;
-    }
-    if (target < 0)
-    {
-        this->sign = MINUS;
-        target = -target;
-    }
-    while (target != 0)
-    {
-        this->mantissa.push_back(target % 10);
-        target /= 10;
-    }
-    this->digit = mantissa.size();
-}
-BigInt::BigInt(int32_t target)
-{
+    static_assert(std::is_integral<T>::value, "Invalid datatype: integral type required.");
     this->mantissa.clear();
     this->sign = PLUS;
     if (target == 0)
